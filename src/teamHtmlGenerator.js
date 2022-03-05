8d5970e5526776a5ab6e2dd7629eb75c1fc95b86
Yasmin <yasmin.bunnie@gmail.com>
2022-03-05 23:16:00 +0000

    first card attempt

--- src/teamHtmlGenerator.js
+++ src/teamHtmlGenerator.js
@@ -9,17 +9,14 @@ const generateTeamMembersString = (arrayOfTeamMembers) => {
   const finalString = "";
   console.log("Team members to generate....:", arrayOfTeamMembers);
 
-  //TODO:
-  // Loop over the array of team members??
-  // If manager -> generate one type of string literal for that manager (so on for each employee)
-  //Each member string literal -> concatenate with string on line 4. Do I do a ForEach? What kind of loop?
-  //How Do I traverse and get information im looking for?
+  // This dynamically implements the users answers into html strings.
+  // Currently, I have struggled with referencing the information with template literals
 
   if (Manager) {
     return `  <div class="card" style="width: 18rem">
         <div class="card-body">
           <div class="card-header text-light fw-bold bg-primary">
-            <h5 class="card-title">{arrayOfTeamMembers.Manager.name}</h5>
+            <h5 class="card-title">{name Template Literal}</h5>
             <h5 class="card-title">
               <span class="material-icons"> emoji_food_beverage </span> Manager
             </h5>
@@ -30,12 +27,12 @@ const generateTeamMembersString = (arrayOfTeamMembers) => {
           <li class="list-group-item fontsize">
             <span class="fw-bolder">Email:</span>
             <a href="#" class="card-link"
-              >{arrayOfTeamMembers.Manager.email}
+              >{email Template Literal}
             </a>
           </li>
           <li class="list-group-item fontsize">
             <span class="fw-bolder">Office Number:</span>
-            {arrayOfTeamMembers.Manager.officeNumber}
+            {officeNumber Template Literal}
           </li>
         </div>
       </div>
@@ -58,26 +55,30 @@ const generateTeamMembersString = (arrayOfTeamMembers) => {
 // Add extra bootstrap and information and create more complicated strings
 // Finish Other Tests
 //
+// Loop over the array of team members??
+// If manager -> generate one type of string literal for that manager (so on for each employee)
+//Each member string literal -> concatenate with string on line 4. Do I do a ForEach? What kind of loop?
+//How Do I traverse and get information im looking for?
 
 //Function to export to index.js and also call function above to generate the html body
 module.exports = (arrayOfTeamMembers) => {
   return `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Team Dashboard </title>
-    <link rel="stylesheet" href="style.css" />
-</head>
-<body>
-<header>My Team</header>
-    <main>
-  ${generateTeamMembersString(arrayOfTeamMembers)}
-  </main>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title> Team Dashboard </title>
+      <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+      <header>My Team</header>
+      <main>
+        ${generateTeamMembersString(arrayOfTeamMembers)}
+      </main>
  
     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-</html>`;
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+  </html>`;
 };
